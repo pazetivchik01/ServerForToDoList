b31a0ed46d7c89a6a66479953818bfae7e92313a
pazetivchik01 <romanenkodenisandrwivi@gmail.com>
2025-06-19 17:02:53 +0300

    JWT

--- Program.cs
+++ Program.cs
@@ -10,8 +10,26 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<ToDoContext>(options =>
     options.UseMySql(
         builder.Configuration.GetConnectionString("DefaultConnection"),
-        new MySqlServerVersion(new Version(8, 0, 30)) 
+        new MySqlServerVersion(new Version(8, 0, 30))
     ));
+
+builder.Logging.AddConsole().SetMinimumLevel(LogLevel.Debug);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+
 builder.Services.AddControllers();
 var app = builder.Build();
 app.MapControllers();
