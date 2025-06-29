937c86e8b66b8e74d195b07f1a2a2949302b8324
Arep'ew Il'ya <arepiew333@gmail.com>
2025-06-29 20:55:13 +0300

    add help

---
+++ Controllers/HelpController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ServerForToDoList.Controllers
+{
+    [ApiController]
+    [Route("api/help")]
+    public class HelpController : Controller
+    {
+        private readonly IWebHostEnvironment _env;
+
+        public HelpController(IWebHostEnvironment env)
+        {
+            _env = env;
+        }
+
+        [ResponseCache(Duration = 3600)]
+        [HttpGet]
+        public IActionResult GetHelp()
+        {
+            var filePath = Path.Combine(_env.ContentRootPath, "Help", "Guide.html");
+            if (!System.IO.File.Exists(filePath))
+                return NotFound("Справка не найдена");
+
+            return PhysicalFile(filePath, "text/html");
+        }
+    }
+}
